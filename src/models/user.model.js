0935cd85e0c138eb8ab0362bef498206f849d497
Bishoy Hany <bishoyhany31@hotmail.com>
2024-02-10 16:48:25 +0200

    Format file

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,38 +1,38 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const UserSchema = mongoose.Schema({
-    name: {
-        type: String(),
-        unqiue: true,
-        trim: true,
-        required: [true, 'User must have a name']
-    },
-    password: {
-        type: String(),
-        required: [true, 'User must have a password']
-    },
-    email: {
-        type: String(),
-        trim: true,
-        required: [true, 'User must have an email']
-    },
-    passwordChangeAt: Date,
-    picture: {
-        type: String(),
-        trim: true,
-    }
-})
+  name: {
+    type: String(),
+    unqiue: true,
+    trim: true,
+    required: [true, "User must have a name"],
+  },
+  password: {
+    type: String(),
+    required: [true, "User must have a password"],
+  },
+  email: {
+    type: String(),
+    trim: true,
+    required: [true, "User must have an email"],
+  },
+  passwordChangeAt: Date,
+  picture: {
+    type: String(),
+    trim: true,
+  },
+});
 
-UserSchema.pre('save', async function(next) {
-    if(!this.isModified('password')) return next();
-    this.passwordChangeAt = Date.now();
-    this.password = await bcrypt.hash(this.password, 4);
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.passwordChangeAt = Date.now();
+  this.password = await bcrypt.hash(this.password, 4);
 });
 
 UserSchema.methods.comparePasswords = async function (candidatePw) {
-    return await bcrypt.compare(candidatePw, this.password);
-}
+  return await bcrypt.compare(candidatePw, this.password);
+};
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
