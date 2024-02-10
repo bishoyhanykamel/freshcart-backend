c8753ffb88559790d88b2625fd9a2c0a9c89215b
Bishoy Hany <bishoyhany31@hotmail.com>
2024-02-10 16:27:07 +0200

    Modify mongoose version

--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+dotenv.config({ path: './config.env' });
 
 //////////////////////////////////////////////////
 // Database connection
 mongoose.connect(process.env.DATABASE_LOCAL, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 }).then((con) => console.log('[Database]: connection successful'))
 .catch(err => console.log('[Database]: connection error: ', err));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, () => {
   console.log(`Fresh cart back-end started, listening on port: ${PORT}`);
