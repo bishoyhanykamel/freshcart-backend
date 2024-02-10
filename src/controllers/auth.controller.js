142f419a7966e14dca683d387594eb59ef2efd9a
Bishoy Hany <bishoyhany31@hotmail.com>
2024-02-10 16:55:05 +0200

    Add initial login endpoint

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const UserRepository = require("./../models/user.model");
+const jwt = require('jsonwebtoken');
 
 exports.signup = async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -22,15 +23,36 @@ exports.login = async (req, res, next) => {
   // 1) Check body
   const { email, password } = req.body;
   if (!email || !password) {
+    // TODO: global error handling
     return res.status(400).json({
       status: "400",
-      message: "Missing login credentials",
+      message: "Missing login credentials.",
     });
   }
 
   // 2) check if user exists
   const currentUser = await User.findOne({ email }).select("+password");
   // 3) check password
+  if (!currentUser || !(await user.comparePasswords(password))) {
+    // TODO: global error handling
+    return res.status(401).json({
+      status: "401",
+      message: "Invalid login credentials.",
+    });
+  }
 
   // 4) return token and save user in request object for future reference
+  const token = generateToken(currentUser._id);
+  return res.status(200).json({
+    status: "200",
+    message: "Successfully logged in.",
+    token,
+  });
 };
+
+
+const generateToken = id => {
+    return jwt.sign({id}, process.env.JWT_SECRET_KEY, {
+        expiresIn: process.env.JWT_EXPIRE_IN
+    })
+}
