7e88c41c1efac68aac9241a2aa6d1f4c3877f875
Bishoy Hany <bishoyhany31@hotmail.com>
2024-02-10 16:23:29 +0200

    Add database connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
-const app = require('./app');
-const dotenv = require('dotenv');
+const app = require("./app");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+//////////////////////////////////////////////////
+// Database connection
+mongoose.connect(process.env.DATABASE_LOCAL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}).then((con) => console.log('[Database]: connection successful'))
+.catch(err => console.log('[Database]: connection error: ', err));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Fresh cart back-end started, listening on port: ${PORT}`);
-})
+  console.log(`Fresh cart back-end started, listening on port: ${PORT}`);
+});
