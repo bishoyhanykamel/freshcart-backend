72fce17ab46d65bb3bc2ec64a3f3690809fc78f4
Bishoy Hany <bishoyhany31@hotmail.com>
2024-02-11 12:54:33 +0200

    Secure error message details for production

--- src/controllers/error.controller.js
+++ src/controllers/error.controller.js
@@ -1,14 +1,13 @@
 const handleError = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "Server failure.";
-  
-  if(process.env.NODE_ENV === 'dev') {
+
+  if (process.env.NODE_ENV === "dev") {
     sendErrorDev(err, res);
-  } else if(process.env.NODE_ENV === 'production') {
+  } else if (process.env.NODE_ENV === "production") {
     sendErrorProduction(err, res);
   }
 };
-
 
 const sendErrorDev = (err, res) => {
   return res.status(err.statusCode).json({
@@ -17,13 +16,20 @@ const sendErrorDev = (err, res) => {
     errorStack: err.stack,
     error: err,
   });
-}
+};
 
 const sendErrorProduction = (err, res) => {
-  return res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-}
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    return res.status(500).json({
+      status: "Error",
+      message: "Sorry, something went wrong!",
+    });
+  }
+};
 
 module.exports = handleError;
