bf7689c8b198606ab5880e6b51669f89df40fd70
Bishoy Hany <bishoyhany31@hotmail.com>
2024-02-11 12:52:23 +0200

    Different error responses for node_env

--- src/controllers/error.controller.js
+++ src/controllers/error.controller.js
@@ -1,11 +1,29 @@
 const handleError = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "Server failure.";
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  
+  if(process.env.NODE_ENV === 'dev') {
+    sendErrorDev(err, res);
+  } else if(process.env.NODE_ENV === 'production') {
+    sendErrorProduction(err, res);
+  }
 };
 
+
+const sendErrorDev = (err, res) => {
+  return res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    errorStack: err.stack,
+    error: err,
+  });
+}
+
+const sendErrorProduction = (err, res) => {
+  return res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+}
 
 module.exports = handleError;
