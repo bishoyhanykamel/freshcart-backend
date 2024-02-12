14cfdcc479d83fbcad16a459060ffbb93ab12c1a
Bishoy Hany <bishoyhany31@hotmail.com>
2024-02-12 15:58:11 +0200

    Add products to application routes middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/error.controller");
 const authRouter = require("./routes/auth.route");
 const categoriesRouter = require("./routes/category.route");
+const productsRouter = require("./routes/product.route");
 const app = express();
 
 //////////////////////////////////////////////////
@@ -11,8 +12,10 @@ app.use(express.json());
 
 /////////////////////////////////////////////////
 // Routes
-app.use("/api/v1/users", authRouter);
-app.use("/api/v1/categories", categoriesRouter);
+app
+  .use("/api/v1/users", authRouter)
+  .use("/api/v1/categories", categoriesRouter)
+  .use("/api/v1/products", productsRouter);
 
 // Handle non-existant routes
 app.all("*", (req, res, next) => {
