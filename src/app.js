7513bda9bf42f68fa064382c73528b3b4c05f2f9
Bishoy Hany <bishoyhany31@hotmail.com>
2024-02-12 15:09:20 +0200

    Add categories router middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/error.controller');
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/error.controller");
 const authRouter = require("./routes/auth.route");
+const categoriesRouter = require("./routes/category.route");
 const app = express();
 
 //////////////////////////////////////////////////
@@ -11,10 +12,11 @@ app.use(express.json());
 /////////////////////////////////////////////////
 // Routes
 app.use("/api/v1/users", authRouter);
+app.use("api/v1/categories", categoriesRouter);
 
 // Handle non-existant routes
 app.all("*", (req, res, next) => {
-  next(new AppError('Path not found.', 404));
+  next(new AppError("Path not found.", 404));
 });
 
 app.use(globalErrorHandler);
