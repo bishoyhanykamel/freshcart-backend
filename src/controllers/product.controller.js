48a8df7f498d54a4ad56954daaec8685ef06cac9
Bishoy Hany <bishoyhany31@hotmail.com>
2024-02-12 16:04:40 +0200

    Initial advanced filtration for products

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,18 +1,29 @@
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
-const statusCodes = require('http-status-codes').StatusCodes;
-const ProductRepository = require('./../models/product.model');
-
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+const statusCodes = require("http-status-codes").StatusCodes;
+const ProductRepository = require("./../models/product.model");
 
 module.exports.getAllProducts = catchAsync(async (req, res, next) => {
-    const products = await ProductRepository.find();
+  const searchQuery = { ...req.query };
+  const excludedFilters = ["page", "sort", "limit", "fields"];
+  excludedFilters.forEach((filter) => delete searchQuery[filter]);
 
-    res.status(statusCodes.OK).json({
-        status: 'success',
-        message: 'Retrieved all products',
-        length: products.length,
-        data: {
-            products
-        }
-    });
+  // Advanced filtering: GTE/GT/LTE/LT, map them to $gte for mongoDB
+  let searchParams = JSON.stringify(searchQuery);
+  searchParams.replace(/(gt|gte|lt|lte)/g, (param) => `$${param}`);
+  searchParams = JSON.parse(searchParams);
+
+  let productsQuery = searchParams
+    ? ProductRepository.find(searchParams)
+    : ProductRepository.find();
+
+  const products = await productsQuery;
+  res.status(statusCodes.OK).json({
+    status: "success",
+    message: "Retrieved all products",
+    length: products.length,
+    data: {
+      products,
+    },
+  });
 });
