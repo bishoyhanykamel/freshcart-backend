5de0c136377c4f60e87a12cc9c5482888f135296
Bishoy Hany <bishoyhany31@hotmail.com>
2024-02-12 15:01:47 +0200

    Add initial category model

---
+++ src/models/category.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.schema({
+    name: {
+        type: String,
+        trim: true,
+        required: [true, 'Category must have a name']
+    },
+    image: {
+        type: String,
+        required: [true, 'Category must have an image'],
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    updatedAt: {
+        type: Date
+    }
+});
+
+const CategoriesModel = mongoose.model("Categories", schema);
+
+module.exports = CategoriesModel;
